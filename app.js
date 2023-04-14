742ee46c0210db47bd2ed15a2efc15309c200d8d
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-04-14 15:28:48 -0300

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var five = require("johnny-five");
+var firebase = require("firebase");
+
+var board = new five.Board();
+
+board.on("ready", function(){
+    var led = new five.Led(13);
+
+    this.repl.inject({led: led});
+
+    var config = {
+        apiKey: "syegr4w2LKqRVWOmIi8V1m6XFyNIgJYxSHL85DpX",
+        authDomain: "lampada-f83c6-default-rtdb.firebaseapp.com",
+        databaseURL: "https://lampada-f83c6-default-rtdb.firebaseio.com/",
+        storageBucket: "lampada-f83c6-default-rtdb.appspot.com",
+      };
+      firebase.initializeApp(config);
+
+      var starCountRef = firebase.database().ref('lampada').on('value', function(snapshot){
+        let lampada = snapshot.val();
+        if(lampada == 'on'){
+            led.on();
+        }else{
+            led.off();
+        }
+      });
+
+})
