377916808faad3951e291c123ff4a65301950637
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-04-18 17:35:31 -0300

    commit

--- teste.js
+++ teste.js
@@ -1,11 +1,3 @@
-
-var five = require("johnny-five");
-const board = new five.Board();
-board.on("ready", () => {
-    const led = new five.Led(13);
-});
-
-
 function mudaImg(atributo){
     if (atributo == 'img/lampadaoff.png'){
         document.getElementById("lampada").src = 'img/lampadaon.png';
