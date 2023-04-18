273ece1acd3172a3cfb861f6e58e8f097b8f65a8
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-04-18 11:36:45 -0300

    commit

--- teste.js
+++ teste.js
@@ -1,18 +1,17 @@
-function ligaled(){
-    var five = require("johnny-five");
-    const board = new five.Board();
-    board.on("ready", () => {
-        const led = new five.Led(13);
-        led.on();
-      });
-}
+
+var five = require("johnny-five");
+const board = new five.Board();
+board.on("ready", () => {
+    const led = new five.Led(13);
+});
 
 
 function mudaImg(atributo){
     if (atributo == 'img/lampadaoff.png'){
         document.getElementById("lampada").src = 'img/lampadaon.png';
-        ligaled();
+        led.on();
     }else {
         document.getElementById("lampada").src = 'img/lampadaoff.png';
+        led.off();
     }
 }
