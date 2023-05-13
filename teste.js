7d333625b8ef3e063d15b1c139d0169375d29a9b
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-05-13 16:51:40 -0300

    commit

--- teste.js
+++ teste.js
@@ -1,17 +1,17 @@
 function mudaImg(atributo){
     if (atributo == 'img/lampadaoff.png'){
         document.getElementById("lampada").src = 'img/lampadaon.png';
-        firebase.database().ref('MeuPrimeiroProjeto/lampada').set('on');
+        firebase.database().ref('MeuPrimeiroProjeto/lampada').set('true');
     }else {
         document.getElementById("lampada").src = 'img/lampadaoff.png';
-        firebase.database().ref('MeuPrimeiroProjeto/lampada').set('off');
+        firebase.database().ref('MeuPrimeiroProjeto/lampada').set('false');
     }
 }
 var flag = true;
 function teste(){
     if (flag == true){
         firebase.database().ref('MeuPrimeiroProjeto/lampada').on('value', snapshot => {let dados = snapshot.val();
-            if (dados == 'on'){
+            if (dados == 'true'){
                 document.getElementById("lampada").src = 'img/lampadaon.png';
             }else {
                 document.getElementById("lampada").src = 'img/lampadaoff.png';
